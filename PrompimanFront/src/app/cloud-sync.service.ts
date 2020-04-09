7ed96653fff840a138631f912591559d98470061
woraput <woraput@perfenterprise.com>
2020-04-09 16:57:22 +0700

    add api createMember

--- PrompimanFront/src/app/cloud-sync.service.ts
+++ PrompimanFront/src/app/cloud-sync.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Member } from 'src/models/Member';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +14,34 @@ export class CloudSyncService {
     console.log('Create CloudSyncProvider Provider');
   }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'accept': 'text/plain',
+      'Content-Type': 'application/json-patch+json'
+    })
+  }
+
   public getByID(_id: string): any {
     return this.http.get<Member>(this.baseUrl + 'Member/GetById/' + _id);
   }
-  
-  public getuser(page: number, size: number){
-    return this.http.get<Member[]>("http://localhost:5000/api/Member/Get/"+page+"/"+size+"?word=%20");
+
+  public getuser(page: number, size: number) {
+    return this.http.get<Member[]>("http://localhost:5000/api/Member/Get/" + page + "/" + size + "?word=%20");
+  }
+
+  public createMember(member: Member): Observable<any> {
+    console.log(JSON.stringify(member));
+    console.log(member);
+    console.log({ member });
+
+    return this.http.post<any>(this.baseUrl + "Member/Create/", JSON.stringify(member), this.httpOptions);
+  }
+
+  public createTh(member: Member) {
+    return this.http.post(this.baseUrl + "Member/CreateTn/", { member });
+  }
+
+  public createEn(member: Member) {
+    return this.http.post(this.baseUrl + "Member/CreateEn/", { member });
   }
 }
